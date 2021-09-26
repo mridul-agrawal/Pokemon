47c9541673f5a8f1fa1d6bc27107fa2717c99cf6
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-09-26 21:50:44 +0530

    Created Scripts for HP Bar and Battle HUD.

---
+++ Assets/Scripts/Battle/BattleHUD.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    [SerializeField] Text NameText;
+    [SerializeField] Text LevelText;
+    [SerializeField] HPBar hpBar;
+
+    public void SetData(Pokemon pokemon)
+    {
+        NameText.text = pokemon.Base.Name;
+        LevelText.text = "Lvl" + pokemon.Level;
+        hpBar.SetHP((float) pokemon.HP / pokemon.MaxHP);
+    }
+}
