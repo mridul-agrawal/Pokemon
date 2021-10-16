ef5429986eb9d579e6d9a9855a0a7505de890035
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-10-16 18:09:59 +0530

    Implemented Battle eneter Animation.

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BattleUnit : MonoBehaviour
 {
@@ -11,6 +12,15 @@ public class BattleUnit : MonoBehaviour
 
     public Pokemon Pokemon { get; set; }
 
+    Image image;
+    Vector3 originalPos;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        originalPos = image.transform.localPosition;
+    }
+
     public void Setup()
     {
         Pokemon = new Pokemon(_base, level);
@@ -18,5 +28,16 @@ public class BattleUnit : MonoBehaviour
             GetComponent<Image>().sprite = Pokemon.Base.BackSprite;
         else
             GetComponent<Image>().sprite = Pokemon.Base.FrontSprite;
+        PlayerEnterAnimation();
     }
+
+    public void PlayerEnterAnimation()
+    {
+        if (isPlayerUnit)
+            image.transform.localPosition = new Vector3(-500, originalPos.y);
+        else
+            image.transform.localPosition = new Vector3(500, originalPos.y);
+        image.transform.DOLocalMoveX(originalPos.x, 1f);
+    }
+
 }
